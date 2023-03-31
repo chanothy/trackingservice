3a97689d4f9e2afab373bf0e07f4012d29d368a1
timchan <timchan@iu.edu>
2023-03-31 15:49:59 -0400

    recommit

--- src/main/java/edu/iu/c322/trackingservice/fakedata/TrackingRepository.java
+++ src/main/java/edu/iu/c322/trackingservice/fakedata/TrackingRepository.java
@@ -52,18 +52,6 @@ public class TrackingRepository {
         return orders;
     }
 
-//    public int create(Order order) {
-//        orders.add(order);
-//        int id = orders.size() + 1;
-//        order.setStatus("ordered");
-//
-//        List<Item> items = order.getItems();
-//        for (int i = 0; i < items.size(); i++) {
-//            items.get(i).setItemId(i+1);
-//        }
-//        return order.getCustomerId();
-//    }
-//
     public Order getOrderById(int id) {
         return orders.stream().filter(x -> x.getCustomerId() == id).findAny().orElse(null);
     }
@@ -101,60 +89,4 @@ public class TrackingRepository {
         }
     }
 
-//
-//    public Item getItemById(int id, Order o) {
-//        return o.getItems().stream().filter(x -> x.getItemId() == id).findAny().orElse(null);
-//    }
-//
-//    public void delete(int id) {
-//        Order x = getOrderById(id);
-//        if (x != null) {
-//            x.setStatus("cancelled");
-//        }
-//        else {
-//            throw new IllegalStateException("Order id is not valid");
-//        }
-//    }
-
-
-//    public List<CancelledOrder> findAllCanceled() {
-//        return cancelledOrders;
-//    }
-
-//    public void update(CancelledOrder order) {
-//        CancelledOrder x = getCancelledOrderById(order.getOrderId());
-//        if (x != null) {
-//            x.setItemId(order.getItemId());
-//            x.setReason(order.getReason());
-//        }
-//        else {
-//            throw new IllegalStateException("order id is not valid");
-//        }
-//    }
-
-//    @PutMapping("/return")
-//    public void update(CancelledOrder cancel) {
-//        Order x = getOrderById(cancel.getOrderId());
-//        if (x != null) {
-//            Item i = getItemById(cancel.getItemId(),x);
-//            if (i != null) {
-//                x.getReturns().add((cancel));
-//            }
-//            else {
-//                throw new IllegalStateException("item id is not valid");
-//            }
-//        }
-//        else {
-//            throw new IllegalStateException("order id is not valid");
-//        }
-//    }
-//
-//    public int create(CancelledOrder order) {
-//        cancelledOrders.add(order);
-//        return order.getOrderId();
-//    }
-//
-//    public CancelledOrder getCancelledOrderById(int id) {
-//        return cancelledOrders.stream().filter(x -> x.getOrderId() == id).findAny().orElse(null);
-//    }
 }
